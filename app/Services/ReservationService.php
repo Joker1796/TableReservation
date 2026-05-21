b499f2726d8253721e5bea33dff2375668fc9a86
Joker1796 <yas-shaman@yandex.ru>
2026-05-21 13:36:04 +0300

    change ReservationTest

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Reservation;
+use App\Models\Table;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -21,12 +22,20 @@ class ReservationService
         $reservation->comment = $request->comment ?? null;
         $reservation->date = $request->date;
         $reservation->hours = $request->hours ?? null;
+
+        if ($request->table) {
+            $table = Table::find($request->table);
+            $reservation->table()->associate($table);
+        }
+
         $reservation->save();
 
         if ($request->users) {
             $users = User::find($request->users);
             $reservation->users()->attach($users);
         }
+
+        $reservation->load('table', 'users');
 
         return response($reservation, 200);
     }
@@ -41,6 +50,7 @@ class ReservationService
     public static function attachUser(Reservation $reservation, User $user): Response
     {
         $reservation->users()->attach($user);
+        $reservation->load('users');
 
         return response($reservation, 200);
     }
@@ -48,6 +58,7 @@ class ReservationService
     public static function detachUser(Reservation $reservation, User $user): Response
     {
         $reservation->users()->detach($user);
+        $reservation->load('users');
 
         return response($reservation, 200);
     }
--- tests/Feature/Model/ReservationTest.php
+++ tests/Feature/Model/ReservationTest.php
@@ -3,6 +3,7 @@
 namespace Feature\Model;
 
 use App\Models\Reservation;
+use App\Models\Table;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -11,12 +12,12 @@ class ReservationTest extends TestCase
 {
     use RefreshDatabase;
 
-    const array BASE_ATTRIBUTES = [
+    const array BASE_ARGUMENTS = [
         'comment' => 'test comment',
         'date' => '01.01.2026 00:00:00',
         'hours' => '1',
     ];
-    const array UPDATED_BASIC_ATTRIBUTES = [
+    const array UPDATED_BASIC_ARGUMENTS = [
         'comment' => 'test comment updated',
         'date' => '02.01.2026 10:00:00',
         'hours' => '4',
@@ -33,26 +34,27 @@ class ReservationTest extends TestCase
     {
         $this->acting();
 
-        $response = $this->call('GET', '/api/V1/reservation/create', self::BASE_ATTRIBUTES);
+        $response = $this->call(
+            'GET',
+            '/api/V1/reservation/create',
+            self::BASE_ARGUMENTS
+        );
 
         $response->assertOk();
     }
 
-    public function test_reservation_with_one_user_created_successfully(): void
+    public function test_reservation_created_not_successfully_code_302(): void
     {
-        $this->acting();
+        $response = $this->call(
+            'GET',
+            '/api/V1/reservation/create',
+            self::BASE_ARGUMENTS
+        );
 
-        $user = User::factory()->create();
-
-        $arguments = self::BASE_ATTRIBUTES;
-        $arguments['users'] = [$user->id];
-
-        $response = $this->call('GET', '/api/V1/reservation/create', $arguments);
-
-        $response->assertOk();
+        $response->assertStatus(302);
     }
 
-    public function test_reservation_with_two_users_created_successfully(): void
+    public function test_reservation_with_users_created_successfully(): void
     {
         $this->acting();
 
@@ -60,12 +62,41 @@ class ReservationTest extends TestCase
             ->count(2)
             ->create();
 
-        $arguments = self::BASE_ATTRIBUTES;
+        $arguments = self::BASE_ARGUMENTS;
         $arguments['users'] = [$users->pluck('id')->toArray()];
 
-        $response = $this->call('GET', '/api/V1/reservation/create', $arguments);
+        $response = $this->call(
+            'GET',
+            '/api/V1/reservation/create',
+            $arguments
+        );
 
         $response->assertOk();
+    }
+
+    public function test_reservation_with_table_created_successfully(): void
+    {
+        $this->acting();
+
+        $table = Table::factory()->create();
+
+        $arguments = self::BASE_ARGUMENTS;
+        $arguments['table'] = $table->id;
+
+        $response = $this->call(
+            'GET',
+            '/api/V1/reservation/create',
+            $arguments
+        );
+
+        $response->assertOk();
+
+        $content = json_decode($response->getContent(), true);
+
+        $this->assertArrayHasKey('table_id', $content);
+        $this->assertEquals($table->id, $content['table_id']);
+        $this->assertArrayHasKey('table', $content);
+        $this->assertEquals($table->id, $content['table']['id']);
     }
 
     public function test_reservation_updated_successfully(): void
@@ -74,10 +105,27 @@ class ReservationTest extends TestCase
 
         $reservation = Reservation::factory()->create();
 
-        $response = $this->call('PUT', '/api/V1/reservation/'.$reservation->id, self::UPDATED_BASIC_ATTRIBUTES);
+        $response = $this->call(
+            'PUT',
+            '/api/V1/reservation/'.$reservation->id,
+            self::UPDATED_BASIC_ARGUMENTS
+        );
 
         $response->assertOk();
-        $response->assertJsonFragment(self::UPDATED_BASIC_ATTRIBUTES);
+        $response->assertJsonFragment(self::UPDATED_BASIC_ARGUMENTS);
+    }
+
+    public function test_reservation_updated_not_successfully_code_302(): void
+    {
+        $reservation = Reservation::factory()->create();
+
+        $response = $this->call(
+            'PUT',
+            '/api/V1/reservation/'.$reservation->id,
+            self::UPDATED_BASIC_ARGUMENTS
+        );
+
+        $response->assertStatus(302);
     }
 
     public function test_reservation_showed_successfully(): void
@@ -86,9 +134,24 @@ class ReservationTest extends TestCase
 
         $reservation = Reservation::factory()->create();
 
-        $response = $this->call('GET', '/api/V1/reservation/'.$reservation->id);
+        $response = $this->call(
+            'GET',
+            '/api/V1/reservation/'.$reservation->id
+        );
 
         $response->assertOk();
+    }
+
+    public function test_reservation_showed_not_successfully_code_302(): void
+    {
+        $reservation = Reservation::factory()->create();
+
+        $response = $this->call(
+            'GET',
+            '/api/V1/reservation/'.$reservation->id
+        );
+
+        $response->assertStatus(302);
     }
 
     public function test_reservation_soft_delete_successfully(): void
@@ -97,13 +160,28 @@ class ReservationTest extends TestCase
 
         $reservation = Reservation::factory()->create();
 
-        $response = $this->call('DELETE', '/api/V1/reservation/'.$reservation->id);
+        $response = $this->call(
+            'DELETE',
+            '/api/V1/reservation/'.$reservation->id
+        );
 
         $response->assertOk();
 
         $content = json_decode($response->getContent(), true);
         $this->assertArrayHasKey('deleted_at', $content);
         $this->assertNotNull($content['deleted_at']);
+    }
+
+    public function test_reservation_soft_delete_not_successfully_code_302(): void
+    {
+        $reservation = Reservation::factory()->create();
+
+        $response = $this->call(
+            'DELETE',
+            '/api/V1/reservation/'.$reservation->id
+        );
+
+        $response->assertStatus(302);
     }
 
     public function test_reservation_attach_user_successfully(): void
@@ -113,9 +191,30 @@ class ReservationTest extends TestCase
         $reservation = Reservation::factory()->create();
         $user = User::factory()->create();
 
-        $response = $this->call('PUT', '/api/V1/reservation/'.$reservation->id.'/user/'.$user->id);
+        $response = $this->call(
+            'PUT',
+            '/api/V1/reservation/'.$reservation->id.'/user/'.$user->id
+        );
 
         $response->assertOk();
+
+        $this->assertDatabaseHas('reservation_user', [
+            'user_id' => $user->id,
+            'reservation_id' => $reservation->id,
+        ]);
+    }
+
+    public function test_reservation_attach_user_not_successfully_code_302(): void
+    {
+        $reservation = Reservation::factory()->create();
+        $user = User::factory()->create();
+
+        $response = $this->call(
+            'PUT',
+            '/api/V1/reservation/'.$reservation->id.'/user/'.$user->id
+        );
+
+        $response->assertStatus(302);
     }
 
     public function test_reservation_detach_user_successfully(): void
@@ -126,11 +225,34 @@ class ReservationTest extends TestCase
             ->has(User::factory())
             ->create();
 
-        $responseDetach = $this->call(
+        $userId = $reservation->users()->first()->id;
+
+        $response = $this->call(
             'DELETE',
-            '/api/V1/reservation/'.$reservation->id.'/user/'.$reservation->users()->first()->id
+            '/api/V1/reservation/'.$reservation->id.'/user/'.$userId
         );
 
-        $responseDetach->assertOk();
+        $response->assertOk();
+
+        $this->assertDatabaseMissing('reservation_user', [
+            'user_id' => $userId,
+            'reservation_id' => $reservation->id,
+        ]);
+    }
+
+    public function test_reservation_detach_user_not_successfully_code_302(): void
+    {
+        $reservation = Reservation::factory()
+            ->has(User::factory())
+            ->create();
+
+        $userId = $reservation->users()->first()->id;
+
+        $response = $this->call(
+            'DELETE',
+            '/api/V1/reservation/'.$reservation->id.'/user/'.$userId
+        );
+
+        $response->assertStatus(302);
     }
 }
